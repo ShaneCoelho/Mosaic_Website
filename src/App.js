bd905320dcfd0a4bad7bc49a5e5e1d58702da90a
Carl <133379432+FinValuer@users.noreply.github.com>
2023-08-27 15:28:53 +0530

    updated package.json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-
 const App = () => {
   return (
     <div>
-     
+      <div className="container">
+        <h1>My Bootstrap App</h1>
+        <button className="btn btn-primary">Click Me</button>
+      </div>
     </div>
   );
 };
