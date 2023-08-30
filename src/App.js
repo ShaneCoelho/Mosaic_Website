fece89f1d97a4c06c39ea0a0ebb3abeb1c10e57f
prajpurohit680 <prajpurohit680@gmail.com>
2023-08-30 21:30:48 +0530

    events done

--- src/App.js
+++ src/App.js
@@ -7,51 +7,52 @@ import Hero from "./pages/Hero/Hero";
 import About from "./pages/About/About";
 import bg from "./assets/galaxy-bg.jpg";
 import bg2 from "./assets/TechnicalChapter/background.jpg";
+import EventHighlight from "./pages/EventHighlight/EventHighlight";
 
 const App = () => {
 
   window.onload = () => {
     function stars(section, maxHeight, count) {
       var i = 0;
-  
+
       while (i < count) {
         const star = document.createElement('i');
         const x = Math.floor(Math.random() * window.innerWidth);
         const y = Math.floor(Math.random() * maxHeight);
-  
+
         const size = Math.random() * 4;
         star.style.left = x + 'px';
         star.style.top = y + 'px';
         star.style.width = 1 + size + 'px';
         star.style.height = 1 + size + 'px';
-  
+
         const duration = Math.random() * 2;
         star.style.animationDuration = 2 + duration + 's';
         star.style.animationDelay = duration + 's';
-  
+
         section.appendChild(star);
         i++;
       }
     }
-  
+
     const sections = document.querySelectorAll('section');
     sections.forEach((section) => {
       let maxHeight = Math.max(window.innerHeight, document.documentElement.scrollHeight);
-      let starCount = 150; 
-  
+      let starCount = 150;
+
       if (window.innerWidth <= 768) {
-        maxHeight /= 0.5; 
-        starCount =100; 
+        maxHeight /= 0.5;
+        starCount = 100;
       }
-  
+
       stars(section, maxHeight, starCount);
     });
   };
 
- 
+
   return (
     <section class="star-bg">
-    <div>
+      <div>
         {/* style={{ backgroundImage: `url(${bg})`,
   backgroundRepeat: 'no-repeat',
   backgroundSize: 'cover',}} */}
@@ -60,6 +61,7 @@ const App = () => {
         <About />
         <Judges />
         <Mentors />
+        <EventHighlight />
         <TechnicalChapter />
       </div>
     </section>
---
+++ src/pages/EventHighlight/EventHighlight.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import './EventHighlight.css';
+import Planet1 from '../../assets/events/planet-01.png';
+import Planet2 from '../../assets/events/planet-02.png';
+import Planet3 from '../../assets/events/planet-03.png';
+import Planet4 from '../../assets/events/planet-04.png';
+import Planet5 from '../../assets/events/planet-05.png';
+import Planet6 from '../../assets/events/planet-06.png';
+import Planet7 from '../../assets/events/planet-07.png';
+import EventData from './EventData'
+
+const planets = [
+    Planet1,
+    Planet2,
+    Planet3,
+    Planet4,
+    Planet5,
+    Planet6,
+    Planet7,
+]
+
+
+const EventHighlight = () => {
+
+
+    return (
+        <section id='event'>
+            <React.Fragment>
+                {
+                    EventData.map((e, i) => {
+                        return (
+                            <div key={i} >
+                                <div className="row mt-3">
+                                    <div className="glow-text col-12 text-center">{e.title}</div>
+
+                                    <div className="glow-desc col-12 text-center">{e.date}</div>
+                                </div>
+                                {
+                                    e.timeline.map((tl, j) => {
+                                        return (
+                                            <div className="container-fluid" key={j}>
+                                                <div className="timeline">
+                                                    {
+                                                        j % 2 === 0 ?
+                                                            <div className="event left" >
+                                                                <img src={planets[j % 7]} alt="planet" className="event-img" />
+                                                                <div className="event-container">
+                                                                    <div className="event-date">{tl.time}</div>
+                                                                    <div className="event-description">{tl.desc}</div>
+                                                                </div>
+                                                            </div>
+                                                            :
+                                                            <div className="event right">
+                                                                <img src={planets[j % 7]} alt="planet" className="event-img" />
+                                                                <div className="event-container">
+                                                                    <div className="event-date">{tl.time}</div>
+                                                                    <div className="event-description">{tl.desc}</div>
+                                                                </div>
+                                                            </div>}
+
+
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        );
+                    }
+                    )
+                }
+
+            </React.Fragment>
+        </section>
+    )
+}
+
+export default EventHighlight
