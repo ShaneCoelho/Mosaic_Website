887b76a4aa4236e1d3380043506edb1ab3328f48
Darshan Jain <darshanjain768@gmail.com>
2023-08-30 20:51:38 +0530

    animated background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './index.css'
 import Judges from "./pages/Judges/Judges";
 import Mentors from "./pages/Mentors/Mentors";
 import TechnicalChapter from "./pages/TechnicalChapter/TechnicalChapter";
@@ -8,16 +9,60 @@ import bg from "./assets/galaxy-bg.jpg";
 import bg2 from "./assets/TechnicalChapter/background.jpg";
 
 const App = () => {
+
+  window.onload = () => {
+    function stars(section, maxHeight, count) {
+      var i = 0;
+  
+      while (i < count) {
+        const star = document.createElement('i');
+        const x = Math.floor(Math.random() * window.innerWidth);
+        const y = Math.floor(Math.random() * maxHeight);
+  
+        const size = Math.random() * 4;
+        star.style.left = x + 'px';
+        star.style.top = y + 'px';
+        star.style.width = 1 + size + 'px';
+        star.style.height = 1 + size + 'px';
+  
+        const duration = Math.random() * 2;
+        star.style.animationDuration = 2 + duration + 's';
+        star.style.animationDelay = duration + 's';
+  
+        section.appendChild(star);
+        i++;
+      }
+    }
+  
+    const sections = document.querySelectorAll('section');
+    sections.forEach((section) => {
+      let maxHeight = Math.max(window.innerHeight, document.documentElement.scrollHeight);
+      let starCount = 150; 
+  
+      if (window.innerWidth <= 768) {
+        maxHeight /= 0.5; 
+        starCount =100; 
+      }
+  
+      stars(section, maxHeight, starCount);
+    });
+  };
+
+ 
   return (
-    <div style={{ backgroundImage: `url(${bg})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',}} >
-      <Hero />
-      <About />
-      <Judges/>
-      <Mentors/>
-      <TechnicalChapter/>
-    </div>
+    <section class="star-bg">
+    <div>
+        {/* style={{ backgroundImage: `url(${bg})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',}} */}
+
+        <Hero />
+        <About />
+        <Judges />
+        <Mentors />
+        <TechnicalChapter />
+      </div>
+    </section>
   );
 };
 
