5f1cca2b96570fddec468d4edbfa59f21d811c76
Darshan Jain <darshanjain768@gmail.com>
2023-09-01 17:58:54 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -15,36 +15,40 @@ import ContactUs from "./pages/ContactUs/ContactUs";
 const App = () => {
 
   window.onload = () => {
-    function createStar(maxHeight) {
-      const star = document.createElement('i');
-      const x = Math.floor(Math.random() * window.innerWidth);
-      const y = Math.floor(Math.random() * maxHeight);
+    function stars(section, maxHeight, count) {
+      var i = 0;
   
-      const size = Math.random() * 4;
-      star.style.left = x + 'px';
-      star.style.top = y + 'px';
-      star.style.width = 1 + size + 'px';
-      star.style.height = 1 + size + 'px';
+      while (i < count) {
+        const star = document.createElement('i');
+        const x = Math.floor(Math.random() * window.innerWidth);
+        const y = Math.floor(Math.random() * maxHeight);
   
-      const duration = Math.random() * 2;
-      star.style.animationDuration = 2 + duration + 's';
-      star.style.animationDelay = duration + 's';
+        const size = Math.random() * 4;
+        star.style.left = x + 'px';
+        star.style.top = y + 'px';
+        star.style.width = 1 + size + 'px';
+        star.style.height = 1 + size + 'px';
   
-      return star;
+        const duration = Math.random() * 2;
+        star.style.animationDuration = 2 + duration + 's';
+        star.style.animationDelay = duration + 's';
+  
+        section.appendChild(star);
+        i++;
+      }
     }
   
     const sections = document.querySelectorAll('section');
-    const windowHeight = window.innerHeight;
-    const documentHeight = document.documentElement.scrollHeight;
-    const maxHeight = window.innerWidth <= 768 ? 1.001 * windowHeight : Math.max(windowHeight, documentHeight);
-    const starCount = window.innerWidth <= 768 ? 15 : 150;
-  
     sections.forEach((section) => {
-      const fragment = document.createDocumentFragment();
-      for (let i = 0; i < starCount; i++) {
-        fragment.appendChild(createStar(maxHeight));
+      let maxHeight = Math.max(window.innerHeight, document.documentElement.scrollHeight);
+      let starCount = 150; 
+  
+      if (window.innerWidth <= 768) {
+        maxHeight /= 1.001; 
+        starCount =100; 
       }
-      section.appendChild(fragment);
+  
+      stars(section, maxHeight, starCount);
     });
   };
 
